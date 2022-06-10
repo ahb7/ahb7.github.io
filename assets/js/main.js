e7c1bf5a8d746f12617428f3c9aebac9887f32e9
Abdullah Habib Biswas <ahb7@users.noreply.github.com>
2022-06-10 14:47:45 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -214,7 +214,7 @@
 
 					}).trigger('resize.ie-intro-fix');
 				}
-
+/*
 			// Hide intro on scroll (> small).
 				breakpoints.on('>small', function() {
 
@@ -252,6 +252,7 @@
 					});
 
 			});
+			*/
 
 		}
 
